fb77338b1bc5e368c02eaf138b755b8b81d89029
Melinda Cortez <meli.matrix@outlook.com>
2023-04-19 12:35:27 -0700

    Added create a spot endpoint

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -2,8 +2,8 @@
 const express = require('express');
 const { Op } = require('sequelize');
 
-const {requireAuth } = require('../../utils/auth.js');
-const {appendToSpots, findAvg } = require('../../utils/editSpotsArr');
+const { requireAuth } = require('../../utils/auth.js');
+const { appendToSpots, findAvg } = require('../../utils/editSpotsArr');
 
 const { User, Spot, Review, SpotImage } = require('../../db/models');
 
@@ -14,6 +14,45 @@ const { handleValidationErrors } = require('../../utils/validation');
 
 const router = express.Router();
 
+const validateNewSpot = [
+    check('address')
+        .exists({ checkFalsy: true })
+        .isLength({ min: 5 })
+        .withMessage('Street address is required'),
+    check('city')
+        .exists({ checkFalsy: true })
+        .isLength({ min: 1 })
+        .withMessage('City is required'),
+    check('state')
+        .exists({ checkFalsy: true })
+        .isLength({ min: 2 })
+        .withMessage('State is required'),
+    check('country')
+        .exists({ checkFalsy: true })
+        .isLength({ min: 2 })
+        .withMessage('Country is required.'),
+    check('lat')
+        .exists({ checkFalsey: true })
+        .isFloat({ min: -90, max: 90 })
+        .withMessage('Latitude is not valid'),
+    check('lng')
+        .exists({ checkFalsy: true })
+        .isFloat({ min: -180, max: 180 })
+        .withMessage('Longitude is not valid'),
+    check('name')
+        .exists({ checkFalsey: true })
+        .isLength({ max: 50 })
+        .withMessage('Name must be less than 50 characters'),
+    check('description')
+        .exists({ checkFalsey: true })
+        .isLength({ min: 1 })
+        .withMessage('Description is required'),
+    check('price')
+        .exists({ checkFalsey: true })
+        .isLength({ min: 1 })
+        .withMessage('Price per day is required'),
+    handleValidationErrors
+];
 
 
 
@@ -28,17 +67,31 @@ router.get('/current', requireAuth, async (req, res, next) => {
     const Spots = await appendToSpots(spots);
 
 
-    res.json({Spots})
+    res.json({ Spots })
 });
 
 router.get('/', async (req, res, next) => {
     const spotsArr = await Spot.findAll();
 
-   const Spots = await appendToSpots(spotsArr);
+    const Spots = await appendToSpots(spotsArr);
 
     res.json({
         Spots
     });
+});
+
+router.post('/', requireAuth, validateNewSpot, async (req, res, next) => {
+    const user = req.user;
+
+    const { address, city, state, country, lat, lng, name, description, price } = req.body;
+
+
+
+
+    const newSpot = await user.createSpot({ address, city, state, country, lat, lng, name, description, price });
+
+    res.status(201);
+    res.json(newSpot);
 });
 
 
