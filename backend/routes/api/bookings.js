502267fe8618ca6a700cf01e3833bdadfbd467c1
Melinda Cortez <meli.matrix@outlook.com>
2023-04-20 14:47:41 -0700

    Added edit booking endpoint

--- backend/routes/api/bookings.js
+++ backend/routes/api/bookings.js
@@ -9,9 +9,12 @@ const { User, Spot, Review, SpotImage, ReviewImage, Booking } = require('../../d
 
 
 const { check } = require('express-validator');
-const { handleValidationErrors, validateReview, validateReviewEdits } = require('../../utils/validation');
+const { handleValidationErrors, validateReview, validateReviewEdits, validateBooking, conflict } = require('../../utils/validation');
 
 const router = express.Router();
+
+const err = new Error("Booking couldn't be found");
+err.status = 404;
 
 router.get('/current', requireAuth, async (req, res, next) => {
     const bookings = await Booking.findAll({
@@ -35,12 +38,12 @@ router.get('/current', requireAuth, async (req, res, next) => {
             where: {
                 preview: true
             }
-        }); 
+        });
 
         const Spot = spot.toJSON();
         if (url) console.log(url)
-        Spot.previewImage = url.length ? url[0].dataValues.url: null;
-        
+        Spot.previewImage = url.length ? url[0].dataValues.url : null;
+
 
         obj.Spot = Spot;
 
@@ -55,5 +58,42 @@ router.get('/current', requireAuth, async (req, res, next) => {
         Bookings
     })
 });
+
+router.put('/:bookingId', requireAuth, validateBooking, async (req, res, next) => {
+    const booking = await Booking.findByPk(req.params.bookingId);
+
+    const spot = await Spot.findByPk(booking.spotId);
+
+    if (!booking) {
+        return next(err);
+    }
+
+    if (req.user.id !== booking.userId) {
+        return next(forbid);
+    }
+    const { startDate, endDate } = req.body
+
+    const now = Date.now();
+
+    const end = new Date(booking.endDate.split('-').join('.')).getTime();
+
+    if (now > end) {
+        const err = new Error("Past bookings can't be modified");
+        err.status = 403;
+        return next(err);
+    }
+
+
+    const err = await conflict(spot, startDate, endDate);
+
+    if (err !== 'pass') return next(err);
+    else {
+        booking.startDate = startDate;
+        booking.endDate = endDate;
+        await booking.save();
+
+        return res.json(booking);
+    }
+})
 
 module.exports = router;
