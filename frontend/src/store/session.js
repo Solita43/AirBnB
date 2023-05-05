da0d32181616e6cd5c47d7750bad1e8865429841
Melinda Cortez <meli.matrix@outlook.com>
2023-05-05 11:32:15 -0700

    Created and tested session reducer

---
+++ frontend/src/store/session.js
@@ -0,0 +1,53 @@
+import { csrfFetch } from './csrf';
+
+// constant to avoid debugging typos
+const SET_SESSION = 'user/SET_SESSION';
+const REMOVE_SESSION = 'user/REMOVE_SESSION'
+
+//action creator to set user 
+const setSession = (user) => {
+    return {
+        type: SET_SESSION,
+        user
+    };
+};
+
+//action creator to remove user
+const removeSession = () => {
+    return {
+        type: REMOVE_SESSION
+    }
+}
+
+//thunk action creator
+export const loginUser = (userCredentials) => async (dispatch) => {
+    const res = await csrfFetch('/api/session', {
+        method: 'POST',
+        body: JSON.stringify(userCredentials)
+    });
+
+    if (res.ok) {
+        const user = await res.json();
+
+        dispatch(setSession(user));
+
+        return user;
+    }
+};
+
+// state object
+const initialState = {user: null};
+
+// reducer
+const sessionReducer = (state = initialState, action) => {
+    switch (action.type) {
+        case SET_SESSION:
+            return {...action.user };
+        case REMOVE_SESSION:
+            return {...state, user: null};
+        default:
+            return state;
+    }
+};
+
+export default sessionReducer;
