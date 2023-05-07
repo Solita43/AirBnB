527081f82b855a91cd617dc4b1b18db8e9ea994e
Melinda Cortez <meli.matrix@outlook.com>
2023-05-07 15:49:07 -0700

    Converted sign up page to sign up modal.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,30 +1,21 @@
 import React, { useState, useEffect } from "react";
 import { useDispatch } from "react-redux";
-import { Route, Switch } from "react-router-dom";
+import { Switch } from "react-router-dom";
 import * as sessionActions from "./store/session";
-import SignupFormPage from "./components/SignupFormPage";
 import Navigation from "./components/Navigation";
 
 function App() {
   const dispatch = useDispatch();
   const [isLoaded, setIsLoaded] = useState(false);
-
   useEffect(() => {
     dispatch(sessionActions.restoreSession()).then(() => setIsLoaded(true));
   }, [dispatch]);
 
   return (
     <>
-      <Navigation isLoaded />
-      {isLoaded && (
-        <Switch>
-          <Route path="/signup">
-            <SignupFormPage />
-          </Route>
-        </Switch>
-      )}
+      <Navigation isLoaded={isLoaded} />
+      {isLoaded && <Switch></Switch>}
     </>
-
   );
 }
 
--- frontend/src/components/SignupFormModal/index.js
+++ frontend/src/components/SignupFormModal/index.js
@@ -1,11 +1,11 @@
 import React, { useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { Redirect } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { useModal } from "../../context/Modal";
 import * as sessionActions from "../../store/session";
+import "./SignupForm.css";
 
-function SignupFormPage() {
+function SignupFormModal() {
     const dispatch = useDispatch();
-    const sessionUser = useSelector((state) => state.session.user);
     const [email, setEmail] = useState("");
     const [username, setUsername] = useState("");
     const [firstName, setFirstName] = useState("");
@@ -13,8 +13,7 @@ function SignupFormPage() {
     const [password, setPassword] = useState("");
     const [confirmPassword, setConfirmPassword] = useState("");
     const [errors, setErrors] = useState({});
-
-    if (sessionUser) return <Redirect to="/" />;
+    const { closeModal } = useModal();
 
     const handleSubmit = (e) => {
         e.preventDefault();
@@ -28,12 +27,14 @@ function SignupFormPage() {
                     lastName,
                     password,
                 })
-            ).catch(async (res) => {
-                const data = await res.json();
-                if (data && data.errors) {
-                    setErrors(data.errors);
-                }
-            });
+            )
+                .then(closeModal)
+                .catch(async (res) => {
+                    const data = await res.json();
+                    if (data && data.errors) {
+                        setErrors(data.errors);
+                    }
+                });
         }
         return setErrors({
             confirmPassword: "Confirm Password field must be the same as the Password field"
@@ -44,76 +45,72 @@ function SignupFormPage() {
         <>
             <h1>Sign Up</h1>
             <form onSubmit={handleSubmit}>
-                <div className="inputs">
-                    <label htmlFor="email"> Email</label>
+                <label>
+                    Email
                     <input
-                        id='email'
                         type="text"
                         value={email}
                         onChange={(e) => setEmail(e.target.value)}
                         required
                     />
-                </div>
+                </label>
                 {errors.email && <p>{errors.email}</p>}
-                <div className="inputs">
-                    <label htmlFor="username"> Username</label>
+                <label>
+                    Username
                     <input
-                        id='username'
                         type="text"
                         value={username}
                         onChange={(e) => setUsername(e.target.value)}
                         required
                     />
-                </div>
+                </label>
                 {errors.username && <p>{errors.username}</p>}
-                <div className="inputs">
-                    <label htmlFor="first-name">First Name</label>
+                <label>
+                    First Name
                     <input
-                        id='first-name'
                         type="text"
                         value={firstName}
                         onChange={(e) => setFirstName(e.target.value)}
                         required
                     />
-                </div>
+                </label>
                 {errors.firstName && <p>{errors.firstName}</p>}
-                <div className="inputs">
-                    <label htmlFor="last-name">Last Name</label>
+                <label>
+                    Last Name
                     <input
-                        id='last-name'
                         type="text"
                         value={lastName}
                         onChange={(e) => setLastName(e.target.value)}
                         required
                     />
-                </div>
+                </label>
                 {errors.lastName && <p>{errors.lastName}</p>}
-                <div className="inputs">
-                    <label htmlFor="password">Password</label>
+                <label>
+                    Password
                     <input
-                        id='password'
                         type="password"
                         value={password}
                         onChange={(e) => setPassword(e.target.value)}
                         required
                     />
-                    {errors.password && <p>{errors.password}</p>}
-                </div>
-                <div className="inputs">
-                    <label htmlFor="confpass">Confirm Password</label>
+                </label>
+                {errors.password && <p>{errors.password}</p>}
+                <label>
+                    Confirm Password
                     <input
-                        id='confpass'
                         type="password"
                         value={confirmPassword}
                         onChange={(e) => setConfirmPassword(e.target.value)}
                         required
                     />
-                </div>
-                {errors.confirmPassword && <p>{errors.confirmPassword}</p>}
+                </label>
+                {errors.confirmPassword && (
+                    <p>{errors.confirmPassword}</p>
+                )}
                 <button type="submit">Sign Up</button>
             </form>
         </>
     );
 }
 
-export default SignupFormPage;
+export default SignupFormModal;
