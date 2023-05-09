b1034ab757a9012ad544dcbe9a5d3d594e0b4523
Melinda Cortez <meli.matrix@outlook.com>
2023-05-08 17:04:23 -0700

    added tooltip on hover to spot preview imaged

--- frontend/src/components/LandingPage/SpotCard.js
+++ frontend/src/components/LandingPage/SpotCard.js
@@ -7,7 +7,9 @@ function SpotCard({ spot }) {
 
     return (
         <div className='spot-card'>
-            <img src={spot.previewImage} alt='Image preview of home' className='card-img'></img>
+            <div className='tooltip' data-tooltip={spot.name}>
+                <img src={spot.previewImage} alt='Image preview of home' className='card-img' title={`${spot.name}`}></img>
+            </div>
             <div className="rating-location">
                 <p className="card-location">{spot.city}, {spot.state}</p>
                 <p className="card-starRating">{spot.avgRating ? spot.avgRating : 'New'}</p>
